6b91d79081587fea69f2a4818a184bee236d8dad
Emily Dobao <e_dobao@outlook.com>
2020-07-28 14:36:12 -0400

    Starting the layout

--- KWKTipVillage/ViewController.swift
+++ KWKTipVillage/ViewController.swift
@@ -11,11 +11,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateText()
+        Label.alpha = 1.0
         // Do any additional setup after loading the view.
     }
+   
+   
+    func animateText() {
+        UIView.animate(withDuration: 5.0, animations: {
+            self.Label.alpha = 0
+        }, completion: {
+            (Completed : Bool) -> Void in
+            self.animateText()
+//            UIView.animate(withDuration: 1.0, delay: 3.0, options: UIView.AnimationOptions.curveLinear, animations: {
+//                self.Label.alpha = 0
+//            }, completion: {
+//                (Completed: Bool) -> Void in
+//                self.animateText()
+//        })
+        })
+    }
+        
+        
+        
+    }
 
-    var hello = "hello"
-}
 
